8daf2f68ea1e71a7a4b4d89fa664fc409c8abe16
shivam <vermashivam0051@gmail.com>
2025-06-04 23:01:11 +0530

    donw with addproduct section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,10 @@ import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
 import SellerLogin from './components/seller/SellerLogin';
 import SellerLayout from './pages/seller/SellerLayout';
+import AddProduct from './pages/seller/AddProduct';
+import ProductList from './pages/seller/ProductList';
+import Orders from './pages/seller/Orders';
+
 
 
 
@@ -43,6 +47,9 @@ const App = () => {
             <Route path='/add-address' element={<AddAddress/>}/>
             <Route path='/my-orders' element={<MyOrders/>}/>
             <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+            <Route index element={isSeller ? <AddProduct/> : null} />
+            <Route path='product-list' element={<ProductList/> }/>
+            <Route path='orders' element={<Orders/> }/>
 
             </Route>
         </Routes>
